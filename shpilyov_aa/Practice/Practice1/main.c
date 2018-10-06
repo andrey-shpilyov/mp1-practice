8a51aada7397d2cfff1e798c7c304fcf94d81ae1
andrey-shpilyov <shpolend@yandex.ru>
2018-10-06 03:01:27 +0300

    Shkaf

--- shpilyov_aa/Practice/Practice1/main.c
+++ shpilyov_aa/Practice/Practice1/main.c
@@ -1,20 +1,16 @@
 #include<stdio.h>
 #include<locale.h>
-
 void main()
 {
 	setlocale(LC_ALL, "Rus");
-
 	int p_dcp = 750, p_dvp = 950, p_derevo = 550, kolvo_polok;
 	float M, m_zad_stenok, m_bok_stenok, m_krishok, m_dveri, m_polok, h, w, d, tol_1 = 0.005, tol_2 = 0.015, tol_3 = 0.01;
-
 	printf("Введите высоту шкафа,(м): ");
 	scanf("%f",&h);
 	printf("Введите ширину шкафа,(м): ");
 	scanf("%f", &w);
 	printf("Введите глубину шкафа,(м): ");
 	scanf("%f", &d);
-
 	if ((h >= 1.8f) && (h < 2))
 		kolvo_polok = 4;
 	else if ((h >= 2) && (h <= 2.2f))
@@ -24,7 +20,6 @@ void main()
 		printf("Высота указана неверно!");
 		return;
 	}
-
 	if ((h >= 1.8f) && (h <= 2.2f) && (w >= 0.8f) && (w <= 1.2f) && (d >= 0.5f) && (d <= 0.9f))
 	{
 		m_zad_stenok = 2 * p_dvp * h * w * tol_1;
@@ -40,5 +35,4 @@ void main()
 		printf("Неверные данные!");
 		return;
 	}
-
 }
